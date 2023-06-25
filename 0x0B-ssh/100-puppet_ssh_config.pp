69883f59298909edcf70f1d5d4351b400dd4e180
chimekinglsey <onlinekingsley@gmail.com>
2023-06-25 23:41:39 +0000

    new commit

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,8 +2,5 @@
 file_line { 'disable password login':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
-
-  'add path to find the keys':
-  path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
 }
