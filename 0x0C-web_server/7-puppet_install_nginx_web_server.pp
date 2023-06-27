77702d99c0501a414fcc10c8136b2eac08c702d2
chimekinglsey <onlinekingsley@gmail.com>
2023-06-27 20:23:34 +0000

    new commit

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,42 +1,9 @@
 #!/usr/bin/env bash
 # Install NGINX package
-package { 'nginx':
-  ensure => 'installed',
-}
 
-# Configure NGINX server
-file { '/etc/nginx/sites-available/default':
-  ensure => 'file',
-  mode   => '0644',
-  content => '
-    server {
-      listen 80;
-      server_name _;
-
-      location / {
-        return 200 "Hello World!";
-      }
-
-      location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=MjoP9GWgxe0&t=1592s
-      }
-    }
-  ',
-  require => Package['nginx'],
-}
-
-# Enable default NGINX site
-file { '/etc/nginx/sites-enabled/default':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-# Restart NGINX service
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  hasstatus => true,
-  require   => File['/etc/nginx/sites-enabled/default'],
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo
+  "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_mehttps:\/\/github.com\/Chimekinglsey permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
 
