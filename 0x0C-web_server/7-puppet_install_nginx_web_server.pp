8229f8022c2eab990cd08b0f9acf9718adb2204e
chimekinglsey <onlinekingsley@gmail.com>
2023-07-04 17:06:33 +0000

    puppet installed

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,7 +3,6 @@
 
 exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo
-  "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_mehttps:\/\/github.com\/Chimekinglsey permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me http:\/\/spacedigit.tech\/home.html permanent;/" /etc/nginx/sites-available/default ; sudo service nginx restart; sudo service nginx reload',
 }
 
