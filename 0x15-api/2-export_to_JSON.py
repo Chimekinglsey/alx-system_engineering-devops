46ee73a1ae40c7bdce3d2f6e30900ae1a12b6b94
chimekinglsey <onlinekingsley@gmail.com>
2023-07-25 04:15:28 +0000

    One_check_fail

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,35 +1,39 @@
 #!/usr/bin/python3
-"""
-2. export json
-"""
+"""Get API values and save into a json format"""
 
 import json
 import requests
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-    emp_id = int(sys.argv[1])
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos").json()
-    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/todos"
+    url2 = "https://jsonplaceholder.typicode.com/users"
 
-    userId = ""
-    userName = ""
-    task_status = ""
-    task_title = ""
+    id = int(argv[1])
+
+    todos = requests.get(url).json()
+    users = requests.get(url2).json()
+
+    my_list = []
+
+    name = ""
+    for get in users:
+        if get['id'] == id:
+            name = get['username']
+
+    for todo in todos:
+        if todo['userId'] == id:
+            temp_list = {'task': todo['title'],
+                         'completed': todo['completed'],
+                         'username': name}
+            my_list.append(temp_list)
 
     for user in users:
-        if emp_id == user['id']:
-            userName = user['username']
-            break
+        if user['id'] == id:
+            name = user['username']
 
-    tasks = []
-    for task in todo:
-        if emp_id == task['userId']:
-            userId = task['userId']
-            task_status = str(task['completed'])
-            task_title = task['title']
-            tasks.append({"task": task_title, "completed": task_status,
-                         "username": userName})
-    dictionary = {userId: tasks}
-    with open(f"{userId}.json", 'w') as w:
-        json.dump(dictionary, w)
+    my_dictionary = {id: my_list}
+
+    with open('{}.json'.format(id), 'w', encoding='utf') as file:
+        json.dump(my_dictionary, file)
+
