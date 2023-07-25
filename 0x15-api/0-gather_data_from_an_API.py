50a6e170be4a7163e70e1c2b22eed7caf4a98e96
chimekinglsey <onlinekingsley@gmail.com>
2023-07-25 00:27:25 +0000

    Jsonified

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,11 +8,11 @@ import sys
 
 if __name__ == '__main__':
     emp_id = int(sys.argv[1])
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos")
-    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
     tasks_completed = 0
     total_tasks = 0
-    emp_name = None
+    emp_name = ""
     task_title = []
     for user in todo:
         if user['userId'] == emp_id:
