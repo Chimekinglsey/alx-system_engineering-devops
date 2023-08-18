0a4ba0ac754abd9245c0b9827eca4b7abd96aecf
chimekinglsey <onlinekingsley@gmail.com>
2023-08-18 01:16:41 +0000

    New Commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
-# increases the ulimit of nginx default to accommade high volume of requests
-
-exec {'/etc/default/nginx':
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    command => "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 2048\"' /etc/default/nginx"
+# Increases the ulimit of nginx default to accommodate a high volume of requests
+file { '/etc/default/nginx':
+    ensure  => present,
+    content => "ULIMIT=\"-n 2048\"\n",
 }
+
 exec { 'reload_nginx':
     command     => '/etc/init.d/nginx reload',
     refreshonly => true,
