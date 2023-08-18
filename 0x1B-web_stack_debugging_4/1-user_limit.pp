7fcd9dc737b47c6d468861dd051c15a8b4e91f13
chimekinglsey <onlinekingsley@gmail.com>
2023-08-18 01:22:26 +0000

    increase resource size debugging

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Fix problem of high amount files opened
+
+exec {'find_and_replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 20000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
+}
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 30000/" /etc/security/limits.conf',
+}
